648f676f7d6bc4c0d044c8ec8da5280be6727133
Matthew Meredith <merematt@udel.edu>
2023-03-07 10:28:37 -0500

    venv and stuff

--- crafting_converter.py
+++ crafting_converter.py
@@ -9,11 +9,13 @@ import getopt
 import oyaml as yaml
 import os
 
+
 def main():
     force = False
     novalidate = False
 
-    opt, args = getopt.getopt(sys.argv[1:], "fnh", ["force", "novalidate", "help"])
+    opt, args = getopt.getopt(sys.argv[1:], "fnh", [
+                              "force", "novalidate", "help"])
     args = args[1:]
 
     for o, a in opt:
@@ -24,8 +26,7 @@ def main():
     Valid Options:
         -h | --help       : Show this help menu
         -f | --force      : Overwrite backups if they exist
-        -n | --novalidate : Don't validate item names
-            """)
+        -n | --novalidate : Don't validate item names""")
             return
         if o == "-f" or o == "--force":
             force = True
@@ -35,20 +36,18 @@ def main():
             novalidate = True
             continue
 
-    print(force, novalidate)
-    return
-
     if args is None or len(args) == 0:
         arg: str = input("Enter a filename (or RETURN for all): ")
-        
+
         if arg is None or len(arg) == 0:
-            args = list(filter(lambda file: not file.endswith(".backup"), os.listdir(os.getcwd())))
+            args = list(filter(lambda file: not file.endswith(
+                ".backup"), os.listdir(os.getcwd())))
         else:
             args = []
             while arg is not None and len(arg) != 0:
                 args.append(arg)
                 arg = input("Enter a filename (or RETURN to finish): ")
-    
+
     for path in args:
         if not os.path.exists(path) or not os.path.isfile(path):
             print(f"Skipping {path}: no file")
@@ -60,7 +59,8 @@ def main():
                     result: str
 
                     while True:
-                        result = input("backup already exists, overwrite? [y/N] ").lower()
+                        result = input(
+                            "backup already exists, overwrite? [y/N] ").lower()
                         if result == "y" or result == "n" or result == "":
                             break
 
@@ -80,13 +80,13 @@ def main():
                     continue
 
                 print(f"Parsing {path}")
-                
+
                 i = 0
                 for item_name in item_file:
                     if "crafting" in item_file[item_name]["base"].keys():
                         item_file[item_name]["base"].pop("crafting")
                         i += 1
-                
+
                 print(f"Updated {path}: {i} items")
 
             with open(path, 'w', encoding='utf-8') as file:
